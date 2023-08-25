f6552f5ce253a3611ad718f965a012aec386dec8
moonlight8978 <moonlight8978@gmail.com>
2023-08-25 20:17:49 +0700

    Init project using Turbo

---
+++ packages/ui/multi-step-wizard/index.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useRef, useState } from "react";
+import { WizardStep } from "./wizard-step";
+import type { StepDescriptor } from "./wizard-step";
+
+export function MultiStepWizard({
+  steps,
+  onSubmit = () => {},
+}: {
+  steps: StepDescriptor<any>[];
+  onSubmit?: (values: any) => any;
+}): JSX.Element {
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const formIndexToValues = useRef<Map<number, any>>(new Map());
+
+  const onSubmitRef = useRef(onSubmit);
+  onSubmitRef.current = onSubmit;
+
+  useEffect(() => {
+    if (currentStep === steps.length) {
+      onSubmitRef.current(formIndexToValues.current);
+    }
+  }, [currentStep, steps]);
+
+  return steps.map((step, index) => {
+    return (
+      <WizardStep
+        {...step}
+        key={index.toString()}
+        hidden={index !== currentStep}
+        onNextStep={(values) => {
+          formIndexToValues.current.set(index, values);
+          setCurrentStep(index + 1);
+        }}
+      />
+    );
+  });
+}
---
+++ packages/ui/multi-step-wizard/wizard-step.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import type { AnyObject, ObjectSchema } from "yup";
+import { useForm } from "react-hook-form";
+import type { UseFormReturn } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+export interface StepDescriptor<T extends AnyObject> {
+  validationSchema: ObjectSchema<T>;
+  initialValues: T;
+  render: (form: UseFormReturn<T>) => JSX.Element;
+}
+
+export function WizardStep<T extends AnyObject>({
+  initialValues,
+  render,
+  validationSchema,
+  hidden = false,
+  onNextStep,
+}: StepDescriptor<T> & {
+  hidden: boolean;
+  onNextStep: (values: T) => void;
+}): JSX.Element {
+  const form = useForm({
+    defaultValues: initialValues,
+    resolver: yupResolver(validationSchema),
+  });
+
+  const { formState, getValues } = form;
+  const { isValid, isSubmitSuccessful } = formState;
+
+  useEffect(() => {
+    if (isValid && isSubmitSuccessful) {
+      onNextStep(getValues());
+    }
+  }, [isSubmitSuccessful, isValid, onNextStep, getValues]);
+
+  return hidden || render(form);
+}
